f5a86d05d68c7d5ac07ed76e66bffeb52f509b6a
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 16:48:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {};
 playlist['John Lennon'] = "Imagine";
 
 function updatePlaylist(thePlaylist, theArtist, theSong) {
-  thePlaylist.theArtist = theSong;
+  thePlaylist.theArtist = theSong; // returns the added value
   return thePlalist;
 }
 
 function removeFromPlaylist(thePlaylist, theArtist) {
-  delete thePlaylist.theArtist;
+  delete thePlaylist.theArtist; // 
 }
